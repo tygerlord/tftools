6192f126e2feac69a234bb6dd6fc238724202718
Francois Leblanc <fleblanc50@gmail.com>
2020-04-29 16:40:32 +0200

    change balancing mode

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='tftools',  
-    version='0.0.2', 
+    version='0.0.3', 
     description='Basic tools for tensorflow ml',
     long_description=long_description, 
     long_description_content_type='text/markdown',
